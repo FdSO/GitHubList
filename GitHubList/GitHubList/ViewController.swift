8cce81a73109a656b061d23ee2d68f57a13bcf40
Filipe Oliveira <fdso@outlook.com>
2020-08-01 08:04:38 -0400

    improve(Project): Initial project
    #close

---
+++ GitHubList/GitHubList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubList
+//
+//  Created by Filipe Oliveira on 01/08/20.
+//  Copyright © 2020 Filipe Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
